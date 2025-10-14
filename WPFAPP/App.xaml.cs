52eb4e7ada0ca041b5f228a55a717ff957e9469a
CodeSmashing <123628904+CodeSmashing@users.noreply.github.com>
2025-10-14 19:26:06 +0200

    Refactor to use dependency injection and a centralized service provider
    Refactored App.xaml.cs to use dependency injection and a centralized service provider. Now manually starts up the application.

--- WPFAPP/App.xaml.cs
+++ WPFAPP/App.xaml.cs
@@ -1,21 +1,55 @@
-﻿using Models;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+using Models;
 using System.Configuration;
 using System.Data;
 using System.Windows;
 
-namespace WPFAPP
-{
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
-    {
-        protected override void OnStartup(StartupEventArgs e)
-        {
-            base.OnStartup(e);
-            AgendaDbContext context = new AgendaDbContext();
-            AgendaDbContext.Seeder(context);
-        }
-    }
+namespace WPFAPP {
+	/// <summary>
+	/// Interaction logic for App.xaml
+	/// </summary>
+	public partial class App : Application {
+		static public ServiceProvider ServiceProvider {
+			get; private set;
+		}
 
+		static public AgendaUser User {
+			get; set;
+		} = null!;
+
+		static public MainWindow MainWindow {
+			get; private set;
+		}
+
+		protected override void OnStartup(StartupEventArgs e) {
+			base.OnStartup(e);
+
+			// Setup dependency injection
+			var serviceSet = new ServiceCollection();
+
+			serviceSet.AddDbContext<AgendaDbContext>();
+
+			// Setup DbContext as service
+			serviceSet.AddIdentityCore<AgendaUser>(options => {
+				options.Password.RequireDigit = false;
+				options.Password.RequireLowercase = false;
+				options.Password.RequireNonAlphanumeric = false;
+				options.Password.RequireUppercase = false;
+				options.Password.RequiredLength = 3;
+				options.Password.RequiredUniqueChars = 1;
+			}).AddEntityFrameworkStores<AgendaDbContext>();
+
+			serviceSet.AddLogging();
+
+			// Create the service provider which wil be accessible throughout the app
+			ServiceProvider = serviceSet.BuildServiceProvider();
+
+			AgendaDbContext context = new();
+			AgendaDbContext.Seeder(context);
+
+			MainWindow = new(App.ServiceProvider.GetRequiredService<AgendaDbContext>());
+			MainWindow.Show();
+		}
+	}
 }
