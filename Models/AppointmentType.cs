68d965492e2f8537d1a88049831364c657bb2c6f
CodeSmashing <123628904+CodeSmashing@users.noreply.github.com>
2025-10-23 15:43:21 +0200

    Refactor appointment types: introduce enum, update UI templates
    ## Added
    
    - New [AppointmentTypeName.cs](/Models/AppointmentTypeName.cs) enum for storing appointment type names.
    - Relevant migration files.
    
    ## Changed
    
    - In [AppointmentType.cs](/Models/AppointmentType.cs):
      - Removed the dummy/default instance and its references.
      - Adjusted the Name property to:
        - No longer use a StringLength data annotation for validation.
        - Be of the type AppointmentTypeName.
        - Use the custom enum validator, EnumValidation.
      - Adjusted the SeedingData() method to use the new AppointmentTypeName type when setting the Name property of the seeding appointment types.
    - In [AppointmentControl.xaml](/WPFAPP/AppointmentControl.xaml):
      - Added a ComboBox template to the types ComboBox so that only the appointment type name and description are visible to the user.

--- Models/AppointmentType.cs
+++ Models/AppointmentType.cs
@@ -1,20 +1,18 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Models.CustomValidation;
+using System.ComponentModel.DataAnnotations;
 
 namespace Models {
 	public class AppointmentType {
-		// Dummy/default appointment type
-		static public readonly AppointmentType Dummy = new() { Name = "-", Description = "-", Deleted = DateTime.MaxValue };
-
 		public int Id {
 			get; set;
 		}
 
 		[Required(ErrorMessage = "{0} is vereist")]
-		[StringLength(25, MinimumLength = 3, ErrorMessage = "De afspraak type naam moet minstens 3 characters en mag maximum 25 characters bevatten")]
+		[CustomValidation(typeof(EnumValidation), nameof(EnumValidation.ValidateEnum))]
 		[Display(Name = "Afspraak type naam")]
-		public string Name {
+		public AppointmentTypeName Name {
 			get; set;
-		} = string.Empty;
+		}
 
 		[Required(ErrorMessage = "{0} is vereist")]
 		[StringLength(2000, MinimumLength = 3, ErrorMessage = "De omschrijving moet minstens 3 characters en mag maximum 2000 characters bevatten")]
@@ -28,7 +26,6 @@ namespace Models {
 			get; set;
 		} = "#FF000000"; // Color in which the appointment wil be shown, default is black
 
-		[Required(ErrorMessage = "{0} is vereist")]
 		[Display(Name = "Verwijderd")]
 		[DataType(DataType.DateTime)]
 		public DateTime Deleted {
@@ -42,18 +39,14 @@ namespace Models {
 		// Seeding data
 		public static List<AppointmentType> SeedingData() {
 			return new() {
-				// Voeg een dummy AppointmentType toe
-				Dummy,
-
-				// Voeg enkele voorbeeld AppointmentTypes toe
 				new() {
-					Name = "Onderhoud",
+					Name = AppointmentTypeName.Onderhoud,
 					Description = "Algemeen onderhoud"},
 				new() {
-					Name = "Aanleg",
+					Name = AppointmentTypeName.Aanleg,
 					Description = "Aanleggen van tuin" },
 				new() {
-					Name = "Kennismaking",
+					Name = AppointmentTypeName.Kennismaking,
 					Description = "Kennismaking klant" }
 			};
 		}
