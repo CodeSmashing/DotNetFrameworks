394aa597639ae61b991a97d5f95d6e064cfc717d
CodeSmashing <123628904+CodeSmashing@users.noreply.github.com>
2025-11-23 15:13:13 +0100

    Scaffold and customize Identity pages, update user model references, and UI branding
    ## Added
    
    - Add default ASP.NET Core Identity files for these scaffolded and customized pages:
      - Account/Manage/ManageNav
      - Account/Manage/Index
      - Account/Login
      - Account/Register
    - Add relevant migration files.
    
    ## Changed
    
    - In all scaffolded pages:
      - Changed references from IdentityUser to AgendaUser.
      - Commented out usage of the PhoneNumber property (not currently used).
      - Changed Username property references to DisplayName.
    - In [Register.cshtml.cs](/GardenPlanner_Web/Areas/Identity/Pages/Account/Register.cshtml.cs):
      - Commented out code for sending confirmation mail.
    - In [Program.cs](/GardenPlanner_Web/Program.cs):
      - Added service registrations similar to [GardenPlanner_WPF/App.xaml.cs](/GardenPlanner_WPF/App.xaml.cs).
      - Added scoped using statement to seed the database.
    - In [Index.cshtml](/GardenPlanner_Web/Views/Home/Index.cshtml), and [_Layout.cshtml](/GardenPlanner_Web/Views/Shared/_Layout.cshtml):
      - Updated titles to display "GardenPlanner" instead of "GardenPlanner_Web".
    - In [_LoginPartial.cshtml](/GardenPlanner_Web/Views/Shared/_LoginPartial.cshtml):
      - Added fallback to display FirstName if DisplayName is not available.

--- GardenPlanner_Web/Program.cs
+++ GardenPlanner_Web/Program.cs
@@ -1,9 +1,45 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Models;
+using Models.CustomServices;
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("AgendaDbContextConnection") ?? throw new InvalidOperationException("Connection string 'AgendaDbContextConnection' not found.");
+var totpProvider = typeof(PasswordlessLoginTotpTokenProvider<>).MakeGenericType(typeof(AgendaUser));
 
 // Add services to the container.
+builder.Services
+	.AddDbContext<AgendaDbContext>()
+	.AddLogging()
+	.AddDefaultIdentity<AgendaUser>(options => {
+		options.Password.RequireDigit = false;
+		options.Password.RequireLowercase = false;
+		options.Password.RequireNonAlphanumeric = false;
+		options.Password.RequireUppercase = false;
+		options.Password.RequiredLength = 3;
+		options.Password.RequiredUniqueChars = 1;
+		options.Tokens.PasswordResetTokenProvider = "PasswordlessLoginTotpProvider";
+		options.User.RequireUniqueEmail = true;
+		options.SignIn.RequireConfirmedAccount = false;
+	})
+	.AddRoles<IdentityRole>()
+	.AddEntityFrameworkStores<AgendaDbContext>()
+	.AddPasswordlessLoginTotpTokenProvider();
+
+builder.Services.AddRazorPages();
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope()) {
+	var services = scope.ServiceProvider;
+	try {
+		// Seed the database
+		await AgendaDbContext.Seeder(services);
+	} catch (Exception ex) {
+		var logger = services.GetRequiredService<ILogger<Program>>();
+		logger.LogError(ex, "An error occurred while seeding the database.");
+	}
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment()) {
@@ -24,5 +60,6 @@ app.MapControllerRoute(
 	 pattern: "{controller=Home}/{action=Index}/{id?}")
 	 .WithStaticAssets();
 
+app.MapRazorPages();
 
 app.Run();
